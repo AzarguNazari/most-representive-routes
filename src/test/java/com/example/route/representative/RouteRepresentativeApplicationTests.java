95381b97827d108ffcf7f283d7e8a45158089f48
Hazar Gul Nazari <hazar.nazari@favendo.com>
2022-03-14 21:56:31 +0100

    initial commit

---
+++ src/test/java/com/example/route/representative/RouteRepresentativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.route.representative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteRepresentativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
