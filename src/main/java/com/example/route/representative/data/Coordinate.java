4ccd6286d30c6090637d936271eac921e6c8d6d5
Hazar Gul Nazari <hazar.nazari@favendo.com>
2022-03-15 00:32:09 +0100

    create dedicated class for coodinates

---
+++ src/main/java/com/example/route/representative/data/Coordinate.java
@@ -0,0 +1,14 @@
+package com.example.route.representative.data;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coordinate {
+    private double x;
+    private double y;
+}
