42d6d482481ac20bfe190e899de70d873c967099
Hazar Gul Nazari <hazar.nazari@favendo.com>
2022-03-15 01:01:34 +0100

    add CICI script

---
+++ cicd-locally.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+JAVA_VERSION=$(java -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
+
+echo "|--------------------------------------------------------------------------|"
+echo "|  Note: We expect you have already installed Docker and Docker-Compose    |"
+echo "|        If you don't have docker, please install it                       |"
+echo "|--------------------------------------------------------------------------|"
+
+if [ "$JAVA_VERSION" = "17" ]
+then
+
+  # Create Docker Image of application
+  ./mvnw spring-boot:build-image
+
+  ARTIFACT="$(./mvnw org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.artifactId -q -DforceStdout)"
+  VERSION="$(./mvnw org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)"
+
+  docker tag "$ARTIFACT:$VERSION" "nazariazargul/most-representative-route:latest"
+  docker push "nazariazargul/most-representative-route:latest"
+
+
+  # Run the container of application using docker-compose
+  docker-compose up
+
+  echo "service are running..."
+
+else
+  echo "Please change your java version to 11"
+fi
