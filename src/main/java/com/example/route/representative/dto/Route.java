2aea53a97b0a564373c82f9ba6524749a65a7824
Hazar Gul Nazari <hazar.nazari@favendo.com>
2022-03-16 09:32:52 +0100

    make it more readable

--- src/main/java/com/example/route/representative/dto/Route.java
+++ src/main/java/com/example/route/representative/dto/Route.java
@@ -2,6 +2,14 @@ package com.example.route.representative.dto;
 
 import java.util.List;
 
-public record Route(String id, String fromSeq, String toSeq, String fromPort, String toPort, long legDuration, int count, List<Point> linePoints){}
+public record Route(String id,
+                    String fromSeq,
+                    String toSeq,
+                    String fromPort,
+                    String toPort,
+                    long legDuration,
+                    int count,
+                    List<Point> linePoints)
+{}
 
 
